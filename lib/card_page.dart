38d8c7833a61562c682e2e76c51b571e14b12a07
nickname0112 <applepiena024681@gmail.com>
2026-02-25 15:39:29 +0900

    Flutter project setup

--- lib/card_page.dart
+++ lib/card_page.dart
@@ -6,12 +6,20 @@ class CardPage extends StatelessWidget {
   final String email;
   final String phone;
 
+
+  @override
+  Widget build(BuildContext context) { // This is the required concrete implementation
+    return const Center(
+      child: Text(''),
+    );
+  }
+
   CardPage({
    required this.name,
    required this.job,
     required this.email,
     required this.phone,
 
-});
+  });
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -8,76 +8,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputFormPage(),
+      home: HomePage(),
     );
   }
 }
 
-class InputFormPage extends StatefulWidget {
-  @override
-  State<InputFormPage> createState() => _InputFormPageState();
-}
+class HomePage extends StatelessWidget {
 
-class _InputFormPageState extends State<InputFormPage> {
+  final List<String> fruits =[
+    '사과',
+    '오렌지',
+    '바나나',
+    '포도',
+    '딸기',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('명함 정보 입력'),
-        backgroundColor: Colors.yellow,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Text('명함에 들어갈 정보를 입력하세요',
-              style: TextStyle(
-              fontSize: 20
-            ),),
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '이름',
-                  prefixIcon: Icon(Icons.person),
-                enabledBorder: OutlineInputBorder()
-              ),
-            ),
-
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '직업',
-                  prefixIcon: Icon(Icons.work),
-                  enabledBorder: OutlineInputBorder()
-              ),
-            ),
-
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '이메일',
-                  prefixIcon: Icon(Icons.email),
-                  enabledBorder: OutlineInputBorder()
-              ),
-            ),
-
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '전화번호',
-                  prefixIcon: Icon(Icons.phone),
-                  enabledBorder: OutlineInputBorder()
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: (){}, child: Text('명함 만들기'),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white
-            ),)
-          ],
+        appBar: AppBar(
+          title: Text('실습'),
         ),
-      ),
+        body: ListView.builder(itemCount: fruits.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.star),
+              title: Text(fruits[index]),
+            );
+          },
+        )
     );
   }
 }
