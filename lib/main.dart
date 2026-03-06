fa520e055eae8f16367c8365ca2eb65310e67601
nickname0112 <applepiena024681@gmail.com>
2026-03-06 14:33:23 +0900

    나라 소개 앱 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'product_list_page.dart';
+import 'country_list_page.dart';  // 나라 목록 파일
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProductListPage(),
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        )// 앱 전체 테마 색상
+      ),
+      home: CountryListPage(),  // 첫 화면
     );
   }
 }
